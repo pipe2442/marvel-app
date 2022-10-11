22f7fd63d14e6ef3faa9eccea0e6754febfb4302
Luis Felipe Angulo Torres <42988115+pipe2442@users.noreply.github.com>
2022-10-11 10:13:52 -0500

    fix https request

--- src/helpers/fetchComics.tsx
+++ src/helpers/fetchComics.tsx
@@ -1,16 +1,16 @@
 import { Result } from "../interfaces/FetchComicsResponse";
 
 const MARVEL_API_URL =
-  "http://gateway.marvel.com/v1/public/comics?limit=100&ts=1000&apikey=098d87e933e9643eb34564dbb6cc6d55&hash=2f976d2e6c33d1137a4928cb109a6e01";
+  "https://gateway.marvel.com/v1/public/comics?limit=100&ts=1000&apikey=098d87e933e9643eb34564dbb6cc6d55&hash=2f976d2e6c33d1137a4928cb109a6e01";
 
 export const fetchComics = async (): Promise<Result[]> => {
   const response = await fetch(MARVEL_API_URL);
   const comicsList = await response.json();
 
-  // const data = comicsList.data.results.filter((comic: Result) => {
-  //   return comic.thumbnail.path.split("/")[10] !== "image_not_available";
-  // });
+  const data = comicsList.data.results.filter((comic: Result) => {
+    console.log(comic.thumbnail.path.split("/"));
+    return comic.thumbnail.path.split("/")[10] !== "image_not_available";
+  });
 
-  // return data;
-  return comicsList
+  return data;
 };
